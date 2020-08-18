eb655176ab7e90afcea9eda68ee7ec4ea5390727
gaoyaqin <yaqin-gao@foxmail.com>
2020-08-18 17:33:19 +0800

    章节

---
+++ com/ponder/reptiles/book/ChapterList.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_chapter_list(url):
+    result = []
+    resp = requests.get(url)
+    resp.encoding = resp.apparent_encoding
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    list_div = soup.select_one('div[class="pc_list"]')
+    chapter_list = list_div.find_all('a')
+    for chapter in chapter_list:
+        print(chapter)
+        result.append(chapter)
+    return result
+
+
+get_chapter_list("http://www.xuanshu.com/book/37912/")
--- com/ponder/reptiles/book/GrabName.py
+++ com/ponder/reptiles/book/GrabName.py
@@ -5,17 +5,20 @@ url = 'http://www.xuanshu.com/sort/1.html'
 
 
 def grab_name():
+    book_tag = []
     resp = requests.get(url)
     html = resp.text
+    print(html)
     soup = BeautifulSoup(html, 'html.parser')
     books = soup.select('div[class="xname"]')
     i = 0
-    for key in books:
-        i = i+1
-        if i < 2:
+    for book in books:
+        if i < 1:
             continue
         else:
-            print(key)
+            book_tag.append(book.find('a'))
+        i = i+1
+    return book_tag
 
 
 grab_name()
